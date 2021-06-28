c2324e4f41957612830abdac3786ca464f4e4000
Kerry Shetline <kerry@shetline.com>
2021-06-27 22:17:09 -0400

    Add leading zero to text output for consistency.

--- projects/ng-widgets-showcase/src/app/app.component.ts
+++ projects/ng-widgets-showcase/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DateTime } from '@tubular/time';
-import { isAndroid, isIOS, isString } from '@tubular/util';
+import { convertDigits, convertDigitsToAscii, isAndroid, isIOS, isString } from '@tubular/util';
 
 @Component({
   selector: 'app-root',
@@ -19,18 +19,26 @@ export class AppComponent {
 
   format(zone: string = null, locale: string = null, fmt: string | Intl.DateTimeFormatOptions): string {
     const dt = new DateTime(null, zone, locale);
+    let result: string;
+    const base: string[] = [];
+
     dt.taiMillis = this.time;
 
     if (isString(fmt))
-      return dt.format(fmt);
+      result = dt.format(fmt);
+    else {
+      if (zone)
+        fmt.timeZone = zone;
 
-    if (zone)
-      fmt.timeZone = zone;
+      result = new Intl.DateTimeFormat(locale, fmt).format(dt.utcMillis);
 
-    let result = new Intl.DateTimeFormat(locale, fmt).format(dt.utcMillis);
+      if (locale === 'ar' && fmt.era)
+        result = result.replace(/(\d\d) (\d\d) (\d\d\d\d)/, '$3/$2/$1');
+    }
 
-    if (locale === 'ar' && fmt.era)
-      result = result.replace(/(\d\d) (\d\d) (\d\d\d\d)/, '$3/$2/$1');
+    result = convertDigitsToAscii(result, base);
+    result = result.replace(/\b(\d)\b/g, '0$1');
+    result = convertDigits(result, base[0]);
 
     return result;
   }
