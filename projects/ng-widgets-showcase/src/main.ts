3193f530596d98b4bcdc5b0c42c52d44591812df
Kerry Shetline <kerry@shetline.com>
2021-08-12 00:53:11 -0400

    Squeeze width of UTC offset when seconds are displayed.

--- projects/ng-widgets-showcase/src/main.ts
+++ projects/ng-widgets-showcase/src/main.ts
@@ -1,8 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { initTimezoneLarge } from '@tubular/time';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+initTimezoneLarge();
 
 if (environment.production) {
   enableProdMode();
