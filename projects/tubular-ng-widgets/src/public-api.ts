b1c1834d13e32d35e0da0080ac990596d65cd4a7
Kerry Shetline <kerry@shetline.com>
2021-09-11 15:06:18 -0400

    Missing update of public-api.ts.

--- projects/tubular-ng-widgets/src/public-api.ts
+++ projects/tubular-ng-widgets/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/angle-editor/angle-editor.component';
 export * from './lib/calendar-panel/calendar-panel.component';
 export * from './lib/digit-sequence-editor/digit-sequence-editor.directive';
 export * from './lib/form-error-display/form-error-display.component';
+export * from './lib/shrink-wrap/shrink-wrap.component';
 export * from './lib/time-editor/time-editor.component';
 export * from './lib/time-editor/time-editor-limit';
 export * from './lib/tubular-ng-widgets.module';
