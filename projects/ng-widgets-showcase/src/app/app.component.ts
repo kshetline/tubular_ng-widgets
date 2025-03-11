a4c691a4b68fd5426700f77419dfcc1338d62fed
Kerry Shetline <kerry@shetline.com>
2025-03-11 17:21:56 -0400

    Unify some details across branches. Get rid of lodash references.

--- projects/ng-widgets-showcase/src/app/app.component.ts
+++ projects/ng-widgets-showcase/src/app/app.component.ts
@@ -42,7 +42,6 @@ export class AppComponent {
   POSITIVE_ONLY = YearStyle.POSITIVE_ONLY;
   SIGNED = YearStyle.SIGNED;
   TIME_ONLY = DateTimeStyle.TIME_ONLY;
-  isIOS = isIOS();
 
   private _calendarDate: YMDDate;
   private _customLocale = navigator.language;
