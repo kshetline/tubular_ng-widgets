4a2734f657286fa5e4e708bee0ece566c1663b9c
Kerry Shetline <kerry@shetline.com>
2025-03-13 22:09:40 -0400

    Some <tbw-time-editor> unit testing improvements, with a tweak to <tbw-time-editor> to make it easier to test.

--- projects/tubular-ng-widgets/src/lib/time-editor/time-editor.component.spec.ts
+++ projects/tubular-ng-widgets/src/lib/time-editor/time-editor.component.spec.ts
@@ -2,6 +2,7 @@ import { TimeEditorComponent } from './time-editor.component';
 import { Component, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   template: `
@@ -26,11 +27,27 @@ describe('TimeEditorComponent', () => {
 
   async function paste(text: string): Promise<void> {
     timeEditor.doPaste(text);
+    fixture.detectChanges();
     await fixture.whenStable();
   }
 
+  function dseItemSort(a: Element, b: Element): number {
+    const av = parseInt(a.getAttribute('data-name').substring(9));
+    const bv = parseInt(b.getAttribute('data-name').substring(9));
+
+    return av - bv;
+  }
+
+  function readDisplayedText(): string {
+    const elems = Array.from(timeElement.querySelectorAll('[data-name^="dse-item-"]')).sort((a, b) => dseItemSort(a, b));
+
+    return elems.map(elem => elem.textContent?.trim() || '').join('');
+  }
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({}).compileComponents();
+    await TestBed.configureTestingModule({
+      providers: [provideAnimations()]
+    }).compileComponents();
     fixture = TestBed.createComponent(TimeComponent);
     timeComponent = fixture.componentInstance;
     timeEditor = timeComponent.timeEditor;
@@ -43,9 +60,10 @@ describe('TimeEditorComponent', () => {
   describe('should display correct time', () => {
     it('should display correct time', async () => {
       timeEditor.options = 'iso';
+      fixture.detectChanges();
       await paste(sampleTime);
-      console.log(timeEditor.getValueAsText(), timeElement.textContent);
       expect(timeEditor.value).toEqual(new Date(sampleTime).getTime());
+      expect(readDisplayedText()).toEqual(sampleTime);
     });
   });
 });
